9f0594da6b568173296b459dce2fcca409039df6
Navaneeth <navaneethp360@gmail.com>
2025-01-21 13:02:31 +0300

    repository fix

--- src/main/java/com/crud/app/ItemRepository.java
+++ src/main/java/com/crud/app/ItemRepository.java
@@ -1,5 +1,8 @@
 package com.crud.app;
 
-public class ItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
     
 }
